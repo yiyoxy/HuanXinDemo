3b6fcb17b616e196b2e5fd473436ee81d1ddd871
278042465@qq.com <3120205314zzb>
2017-02-21 00:36:37 +0800

    群详情的监听, 跳转到群详情页面

--- app/src/main/java/com/tron/huanxindemo/controller/activity/ChatActivity.java
+++ app/src/main/java/com/tron/huanxindemo/controller/activity/ChatActivity.java
@@ -1,10 +1,15 @@
 package com.tron.huanxindemo.controller.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.FrameLayout;
 
+import com.hyphenate.chat.EMMessage;
+import com.hyphenate.easeui.EaseConstant;
 import com.hyphenate.easeui.ui.EaseChatFragment;
+import com.hyphenate.easeui.widget.chatrow.EaseCustomChatRowProvider;
 import com.tron.huanxindemo.R;
 
 import butterknife.BindView;
@@ -15,6 +20,8 @@ public class ChatActivity extends AppCompatActivity {
     @BindView(R.id.chat_fl)
     FrameLayout chatFl;
 
+    private EaseChatFragment chatFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,16 +29,65 @@ public class ChatActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         initData();
+
+        initListener();
+    }
+
+    private void initListener() {
+        chatFragment.setChatFragmentListener(new EaseChatFragment.EaseChatFragmentHelper() {
+
+            @Override
+            public void onSetMessageAttributes(EMMessage message) {
+
+            }
+
+            @Override
+            public void onEnterToChatDetails() {
+
+                Intent intent = new Intent(ChatActivity.this, ChatDetailsActivity.class);
+
+                intent.putExtra("groupid", getIntent().getExtras().getString(EaseConstant.EXTRA_USER_ID));
+
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onAvatarClick(String username) {
+
+            }
+
+            @Override
+            public void onAvatarLongClick(String username) {
+
+            }
+
+            @Override
+            public boolean onMessageBubbleClick(EMMessage message) {
+                return false;
+            }
+
+            @Override
+            public void onMessageBubbleLongClick(EMMessage message) {
+
+            }
+
+            @Override
+            public boolean onExtendMenuItemClick(int itemId, View view) {
+                return false;
+            }
+
+            @Override
+            public EaseCustomChatRowProvider onSetCustomChatRowProvider() {
+                return null;
+            }
+        });
     }
 
     private void initData() {
 
         // 聊天的Fragment
-        EaseChatFragment chatFragment = new EaseChatFragment();
-
-//        if (getIntent().getExtras() == null){
-//            return;
-//        }
+        chatFragment = new EaseChatFragment();
 
         chatFragment.setArguments(getIntent().getExtras());
 
