bea1e676e82ebf29381013f1af6d0247b857fd94
278042465@qq.com <3120205314zzb>
2017-02-20 15:49:08 +0800

    创建群页面逻辑实现

--- app/src/main/java/com/tron/huanxindemo/controller/activity/CreateGroupActivity.java
+++ app/src/main/java/com/tron/huanxindemo/controller/activity/CreateGroupActivity.java
@@ -8,7 +8,11 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMGroupManager;
+import com.hyphenate.exceptions.HyphenateException;
 import com.tron.huanxindemo.R;
+import com.tron.huanxindemo.model.Model;
 import com.tron.huanxindemo.utils.ShowToast;
 
 import butterknife.BindView;
@@ -28,6 +32,11 @@ public class CreateGroupActivity extends AppCompatActivity {
     @BindView(R.id.bt_newgroup_create)
     Button btNewgroupCreate;
 
+    // 群描述
+    private String groupDesc;
+    // 群名称
+    private String groupName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,24 +47,87 @@ public class CreateGroupActivity extends AppCompatActivity {
 
     @OnClick(R.id.bt_newgroup_create)
     public void onClick() {
-        if (validate()){
+        if (validate()) {
             // 跳转到联系人页面获取联系人数据
             Intent intent = new Intent(CreateGroupActivity.this, PickContactActivity.class);
-            startActivity(intent);
+
+            // 回传数据
+            startActivityForResult(intent, 1); // requestCode为1
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            // 创建群组
+            createGroup(data);
+        }
+    }
+
+    private void createGroup(final Intent data) {
+        Model.getInstance().getGlobalThread().execute(new Runnable() {
+            @Override
+            public void run() {
+                // 1.获得回调的数据: 群成员名称的数组
+                String[] memberses = data.getStringArrayExtra("members");
+
+                // 去环信服务器创建群
+                // 2.获得群设置
+                EMGroupManager.EMGroupOptions groupOptions = new EMGroupManager.EMGroupOptions();
+
+                // 3.设置群最大成员数量
+                groupOptions.maxUsers = 200;
+
+                // 4.设置群类型
+                if (cbNewgroupPublic.isChecked()) {
+                    if (cbNewgroupInvite.isChecked()) {
+                        // 公开群, 任何人都可以加入
+                        groupOptions.style = EMGroupManager.EMGroupStyle.EMGroupStylePublicOpenJoin;
+                    } else {
+                        // 公开群, 需要群主同意才能加入(群主邀请或主动申请)
+                        groupOptions.style = EMGroupManager.EMGroupStyle.EMGroupStylePublicJoinNeedApproval;
+                    }
+                } else {
+                    if (cbNewgroupInvite.isChecked()) {
+                        // 私有群,群成员可以邀请人进群
+                        groupOptions.style = EMGroupManager.EMGroupStyle.EMGroupStylePrivateMemberCanInvite;
+                    } else {
+                        // 私有群,只有群主可以邀请人
+                        groupOptions.style = EMGroupManager.EMGroupStyle.EMGroupStylePrivateOnlyOwnerInvite;
+
+                    }
+                }
+
+                // 5.创建群, 参数: 群名称, 群描述, 要邀请的群成员, 群的其他设置
+                try {
+                    EMClient.getInstance().groupManager()
+                            .createGroup(groupName, groupDesc, memberses, "", groupOptions);
+
+                    ShowToast.showUI(CreateGroupActivity.this, "创建群成功");
+
+                    finish();
+                } catch (HyphenateException e) {
+                    e.printStackTrace();
+                    ShowToast.showUI(CreateGroupActivity.this, "创建群失败" + e.getMessage());
+                }
+            }
+        });
+    }
+
+    // 验证, 获得群名称和群描述
     private boolean validate() {
 
-        String groupDesc = etNewgroupDesc.getText().toString().trim();
-        String groupName = etNewgroupName.getText().toString().trim();
+        groupDesc = etNewgroupDesc.getText().toString().trim();
+        groupName = etNewgroupName.getText().toString().trim();
 
         if (TextUtils.isEmpty(groupDesc)) {
             ShowToast.show(this, "群名称不能为空");
             return false;
         }
 
-        if (TextUtils.isEmpty(groupName)){
+        if (TextUtils.isEmpty(groupName)) {
             ShowToast.show(this, "群简介不能为空");
             return false;
         }
